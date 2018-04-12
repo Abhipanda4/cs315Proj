7c76a47dc9d2903e975ed648914145558be3707b
abhipanda4 <abhipanda03@gmail.com>
2018-04-13 00:51:39 +0530

    Designing added; index almost done

--- index.php
+++ index.php
@@ -1,25 +1,37 @@
 <!doctype html>
 <html lang="en">
 
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="x-ua-compatible" content="ie=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-
-	<title>Simple Database App</title>
-
-	<link rel="stylesheet" href="css/style.css">
-</head>
+<?php include "templates/header.php"; ?>
+<?php
+	if (isset($_SESSION)) {
+	  header("Location: dashboard.php");
+	}
+?>
 
 <body>
-        <?php include "templates/header.php"; ?>
-	<ul>
-		<li><a href="create.php"><strong>Create</strong></a> - add a user</li>
-		<li><a href="read.php"><strong>Read</strong></a> - find a usr</li>
-	</ul>
+	<div class="container main-body">
+		<div class="jumbotron text-center" style="background-color: #337ab7 !important; color: #f7f7f7">
+			<h2> LIBRARY MANAGEMENT SYSTEM </h2>
+		</div>
+		<div class="index-body">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="tile"> <a href="create.php" style="text-decoration: none; color: black;"> Register a New User </a> </div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="tile"> <a href="read.php" style="text-decoration: none; color: black;"> Search Users </a> </div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="tile"> <a href="login.php" style="text-decoration: none; color: black;"> User Login </a> </div>
+				</div>
+			</div>
+		</div>
+	</div>
 	<?php include "templates/footer.php"; ?>
 
 </body>
 </html>
-
-
